93ead29e81a307ea6f61f2753e7a3ecb47a4d363
Md-razvi <48431475+Md-razvi@users.noreply.github.com>
2021-05-05 01:32:33 +0530

    Add files via upload

---
+++ Solution2.java
@@ -0,0 +1,15 @@
+package Loops;
+//Write a java program to print 1 to 20 numbers using the while loop.
+public class Solution2 
+{
+    public static void main(String[] args) 
+    {
+        int i=1;
+        while (i<=20)
+        {
+            System.out.print(i+" ");
+            i=i+1;//or you can write i=i+1
+        }
+
+    }   
+}
