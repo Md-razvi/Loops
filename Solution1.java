795101da2eeb166abe1f22ffa192120b884e2b8e
Md-razvi <48431475+Md-razvi@users.noreply.github.com>
2021-05-05 01:26:32 +0530

    Add files via upload

---
+++ Solution1.java
@@ -0,0 +1,12 @@
+package Loops;
+// Simple program to print “Bright IT Career” ten times using for loop
+public class Solution1 {
+    public static void main(String[] args)
+    {
+        // A simple for loop syntax
+        for (int i=1;i<=10;i++)
+        {
+            System.out.println(i+".Bright IT Career");
+        }
+    }
+}
